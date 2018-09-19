e3fe418994d207f89ebc70b02e0bb17cef1165a1
Lindsay Larkin Larkin <lindsay.larkin@gmail.com>
2018-09-19 10:02:12 -0600

    nodesj.org docs practice

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // Node's modular structure creates modules that remove you from the global scope.  
 // Modules are encapsulated in their local scope. aka They are private and can be made public with export.
 
-const log = require('./logger');
+const os = require('os');
 
-log('message');
+let totalMemory = os.totalmem();
+let freeMemory = os.freemem();
+
+console.log('Total Memory: ' + totalMemory);
+console.log(`Free Memory: ${freeMemory}`);
+
+
