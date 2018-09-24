ba76b53f41c33c2b63830e1a9b0a1287e6a0052d
Lindsay Larkin Larkin <lindsay.larkin@gmail.com>
2018-09-24 13:16:24 -0400

    listeners and events

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 // Node's modular structure creates modules that remove you from the global scope.  
 // Modules are encapsulated in their local scope. aka They are private and can be made public with export.
 
-const os = require('os');
+const EventEmitter = require('events'); //class
+const emitter = new EventEmitter(); //object
 
-let totalMemory = os.totalmem();
-let freeMemory = os.freemem();
+// listener
+emitter.on('messageLogged', function(){
+    console.log('Listener called');
+}) // onn = addListener method
 
-console.log('Total Memory: ' + totalMemory);
-console.log(`Free Memory: ${freeMemory}`);
+// an event needs a listener
+emitter.emit('messageLogged');
 
+// order matters.  need to register the listener before raising the event.
 
